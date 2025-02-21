ada840342d7e020ac6f9e3d64adff87ba72b4be7
Keyner de Avila <kddeavila3@gmail.com>
2025-02-20 20:37:54 -0500

    feat: add the front-end/dist and connect with the back-end

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import type { Coffee } from "../types/coffee";
+
+const API = axios.create({
+    baseURL: "http://localhost:5000",
+    headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "ARC4M"
+    },
+});
+
+export const getCoffees = async (): Promise<Coffee[]> => {
+    try {
+        const response = await API.get<Coffee[]>("/coffes");
+        const data = await response.data;
+        return data;
+
+    } catch (error) {
+        console.error("Error fetching coffees:", error);
+        return [];
+    }
+}
+
+export const getCoffeById = async (id: string): Promise<Coffee | null> => {
+    try {
+        const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3YjM4ZDI0MjQ3YmQzNjRhMTQyNzM5MyIsIm5hbWUiOiJrZGVhdmlsYSIsImVtYWlsIjoia2RkZWF2aWxhM0BnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3NDAxMDEzNDUsImV4cCI6MTc0MDEwNDk0NX0.NR4-KOARXaBhZaIZIJf-Z2w40NF2Qk0z2vI8Ov6i5uI";
+
+        const response = await API.get<Coffee>(`/coffes/${id}`, {
+            headers: {
+                "Authorization": `Bearer ${TOKEN}`
+            }
+        }
+        );
+        const data = await response.data;
+        return data;
+
+    } catch (error) {
+        console.error("Error fetching coffees:", error);
+        return null;
+    }
+}
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+import path from "path";
 import express from 'express';
 import bodyParser from 'body-parser';
 import connectDB from './config/connectDB.js';
 import middleware from './config/middleware.js';
-import coffeRoutes from './routes/coffeRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import coffeRoutes from './routes/coffeRoutes.js';
 import { verifyApiKey } from './utils/auth.js';
 
 const app = express();
@@ -14,17 +15,21 @@ app.use(bodyParser.json());
 
 connectDB();
 
-app.get('/', verifyApiKey, (req, res) => {
-    res.send('Hello  World :)');
-})
+const frontendPath = path.resolve("frontend/dist");
+app.use(express.static(frontendPath));
+
+app.get("/", verifyApiKey, (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+});
+
 app.use('/coffes', coffeRoutes);
 app.use('/users', userRoutes);
 
 try {
     const PORT = process.env.PORT || 5000;
-    app.listen( PORT, () => {
+    app.listen(PORT, () => {
         console.log('-> Server running on port', PORT);
-    } )
+    })
 } catch (e) {
     console.error(e);
 }
